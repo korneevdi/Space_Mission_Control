ff24a8e9f4f5211b7acb86c26e4213c15105142e
Dmitrii Korneev <kdmitry.de@gmail.com>
2025-12-13 16:15:15 +0100

    Created Hibernate config & initialized database

--- src/main/java/spacemissioncontrol/Main.java
+++ src/main/java/spacemissioncontrol/Main.java
@@ -1,8 +1,13 @@
 package spacemissioncontrol;
 
+import org.hibernate.SessionFactory;
+import spacemissioncontrol.util.HibernateConfig;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        try (SessionFactory sessionFactory = HibernateConfig.getSessionFactory()) {
+            System.out.println("Schema created successfully");
+        }
     }
 }
---
+++ src/main/java/spacemissioncontrol/util/HibernateConfig.java
@@ -0,0 +1,23 @@
+package spacemissioncontrol.util;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateConfig {
+
+    private static final SessionFactory sessionFactory;
+
+    static {
+        try {
+            sessionFactory = new Configuration()
+                    .configure("hibernate.cfg.xml")
+                    .buildSessionFactory();
+        } catch (Throwable e) {
+            throw new ExceptionInInitializerError(e);
+        }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+}
